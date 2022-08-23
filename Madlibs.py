f15077adbccee1627f321e97a7bd6ade301c3e7b
gobinddas <48596268+gobinddas@users.noreply.github.com>
2022-08-23 16:04:09 +0545

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,13 @@
+print()
+print(f'Roses are red\n'
+      'Voilet are blur\n'
+      'I love you')
+print()
+print()
+color=input('Enter a color: ')
+plura_name=input('Enter a plural noun: ')
+celebrity=input('Enter a celebrity: ')
+print()
+print('Roses are ' + color )
+print(plura_name + ' are blue')
+print('I love you ' + celebrity)
